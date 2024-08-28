341b5021323648c98b0f2ecddc2a5a33ec280260
zuzazzo <100868689+m1ssha@users.noreply.github.com>
2024-08-28 14:49:13 +0000

    перевод

--- src/containers/CourseCard/components/CourseCardActions/messages.js
+++ src/containers/CourseCard/components/CourseCardActions/messages.js
@@ -4,27 +4,27 @@ const messages = defineMessages({
   upgrade: {
     id: 'learner-dash.courseCard.actions.upgrade',
     description: 'Course card upgrade button text',
-    defaultMessage: 'Upgrade',
+    defaultMessage: 'Обновлениеи',
   },
   beginCourse: {
     id: 'learner-dash.courseCard.actions.beginCourse',
     description: 'Course card begin-course button text',
-    defaultMessage: 'Begin Course',
+    defaultMessage: 'Начать',
   },
   resume: {
     id: 'learner-dash.courseCard.actions.resume',
     description: 'Course card resume button text',
-    defaultMessage: 'Resume',
+    defaultMessage: 'Продолжить',
   },
   viewCourse: {
     id: 'learner-dash.courseCard.actions.viewCourse',
     description: 'Course card view-course button text',
-    defaultMessage: 'View Course',
+    defaultMessage: 'Просмотреть курс',
   },
   selectSession: {
     id: 'learner-dash.courseCard.actions.selectSession',
     description: 'Course card select-session button text',
-    defaultMessage: 'Select Session',
+    defaultMessage: 'Выбрать сессию',
   },
 });
 
